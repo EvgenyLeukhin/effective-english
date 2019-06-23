9720b2bb594147c8d7ca084b4e4e1dd8cf78b104
Evgeny Leukhin <eugeny.leukhin@stellarsolvers.com>
2019-06-23 11:28:47 +0600

    Edit readme.md

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,59 +4,41 @@ module.exports = {
     title: `Effective English`,
     author: `Evgeny Leukhin`,
     description: `Effective English Blog`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    siteUrl: `https://evgenyleukhin.github.io/effective-english/`,
+    social: { twitter: `kylemathews` },
   },
+
   plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
+    { resolve: `gatsby-source-filesystem`,
+      options: { path: `${__dirname}/content/blog`, name: `blog` }},
+
+    { resolve: `gatsby-source-filesystem`,
+      options: { path: `${__dirname}/content/assets`, name: `assets`}},
+
+    { resolve: `gatsby-plugin-typography`,
+      options: { pathToConfigModule: `src/utils/typography` }},
+
+    { resolve: `gatsby-plugin-google-analytics`,
+      options: {}}, // trackingId: `ADD YOUR TRACKING ID HERE`
+
+    { resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
+          { resolve: `gatsby-remark-images`,
+            options: { maxWidth: 590 }},
+
+          { resolve: `gatsby-remark-responsive-iframe`,
+            options: { wrapperStyle: `margin-bottom: 1.0725rem` }},
+
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
+    
+    
+    { resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Effective English Blog`,
         short_name: `Effective English`,
@@ -65,15 +47,12 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-  ],
-}
+      }},
+
+      `gatsby-plugin-feed`,
+      `gatsby-plugin-offline`,
+      `gatsby-plugin-react-helmet`,
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+    ],
+  }
